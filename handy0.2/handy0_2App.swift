1242a6224dfd26a3cb0953dfae2808f578e6a0bd
Shokri Alnajjar <shokre4@hotmail.com>
2022-09-07 14:47:49 +0400

    Initial Commit

---
+++ handy0.2/handy0_2App.swift
@@ -0,0 +1,17 @@
+//
+//  handy0_2App.swift
+//  handy0.2
+//
+//  Created by Shokri Alnajjar on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct handy0_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
